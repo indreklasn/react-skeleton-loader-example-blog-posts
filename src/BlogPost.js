0531d49e59cd381cf81afbb00dab630bdc314b67
Indrek Lasn <lasnindrek@gmail.com>
2020-11-10 11:45:45 +0200

    add profile image

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 
+const imgUrl =
+  "https://cdn-images-1.medium.com/fit/c/50/50/1*Rhzd_aZYgwP2gocFecKi0Q.png";
+
 export default function BlogPost({ title, body, subTitle }) {
   return (
     <div>
+      {body ? (
+        <img src={imgUrl} width="50px" height="50px" alt="profile" />
+      ) : (
+        <Skeleton circle={true} height={50} width={50} />
+      )}
+
       <h1>{title || <Skeleton />}</h1>
       <h3>{subTitle || <Skeleton />}</h3>
-
       <p>{body || <Skeleton count={6} />}</p>
 
       <button>
